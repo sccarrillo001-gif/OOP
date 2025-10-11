7762934b22a6391171c3f1efe62bdca39accce99
sccarrillo001-gif <sccarrillo001@gmail.com>
2025-10-10 21:04:00 -0500

    Update Lab 3.py

--- Lab 3.py
+++ Lab 3.py
@@ -1,27 +1,26 @@
 class Book:
-    def __init__(self, bid, btitle, bauthor, bpublisher, byear):
-        self.bookID = bid
-        self.book_title = btitle
-        self.author_id = bauthor
-        self.publisher = bpublisher
-        self.year_of_publication = byear
-
+    def __init__(self):
+        self.bookID = ""
+        self.book_title = ""
+        self.author_id = ""
+        self.author = ""
+        self.publisher = ""
+        self.year_of_publication = 0
 
     def add_book(self):
         self.bookID = input("Enter the Book ID: ")
         self.book_title = input("Enter the Book Title: ")
-        self.author_id = input("Enter the Book author id: ")
-        self.publisher = input("Enter the Book publisher: ")
-        self.year_of_publication = input("Enter the year of publication: ")
+        self.author_id = input("Enter the Author ID: ")
+        self.publisher = input("Enter the Publisher: ")
+        self.year_of_publication = int(input("Enter the Year of Publication: "))
 
     def display_book(self):
         print("Book Information:")
-        print("Book ID: ", self.bookID)
-        print("Book Title: ", self.book_title)
-        print("Author ID: ", self.author_id)
-        print("Publisher: ", self.publisher)
-        print("Year of publication: ", self.year_of_publication)
-
+        print("Book ID:", self.bookID)
+        print("Book Title:", self.book_title)
+        print("Author:", self.author)
+        print("Publisher:", self.publisher)
+        print("Year of Publication:", self.year_of_publication)
 
 
 class User:
@@ -36,81 +35,124 @@ class User:
     def add_user(self):
         self.userID = input("Enter the User ID: ")
         self.userName = input("Enter the User Name: ")
-        self.password = input("Enter the User password: ")
-        self.address = input("Enter the User address: ")
-        self.email_id = input("Enter the User email id: ")
+        self.password = input("Enter the Password: ")
+        self.address = input("Enter the Address: ")
+        self.email_id = input("Enter the Email ID: ")
 
     def borrow_book(self, book):
         self.books_borrowed.append(book)
-        print(self.userName, "borrowed", book.title)
+        print(self.userName, "borrowed", book.book_title)
 
     def display_user(self):
         print("User Information:")
-        print("User ID: ", self.userID)
-        print("User Name: ", self.userName)
+        print("User ID:", self.userID)
+        print("User Name:", self.userName)
+        print("Email:", self.email_id)
         if not self.books_borrowed:
             print("No books borrowed.")
         else:
             print("Books borrowed:")
             for b in self.books_borrowed:
-                print("Books borrowed: ", b.title, "by", b.author)
+                print("-", b.book_title, "by", b.author)
+
 
 class Author:
     def __init__(self):
-                        self.author_id = ""
-                        self.author_name = ""
-                        self.affiliation = ""
-                        self.country = ""
-                        self.phone = ""
-                        self.email_id = ""
+        self.author_id = ""
+        self.author_name = ""
+        self.affiliation = ""
+        self.country = ""
+        self.phone = ""
+        self.email_id = ""
 
+    def add_author(self):
+        self.author_id = input("Enter Author ID: ")
+        self.author_name = input("Enter Author Name: ")
+        self.affiliation = input("Enter Affiliation: ")
+        self.country = input("Enter Country: ")
+        self.phone = input("Enter Phone: ")
+        self.email_id = input("Enter Email ID: ")
 
-    def add_user(self):
-                    self.userID = input("Enter the User ID: ")
-                    self.userName = input("Enter the User Name: ")
-                    self.password = input("Enter the User password: ")
-                    self.address = input("Enter the User address: ")
-                    self.email_id = input("Enter the User email id: ")
+    def display_author(self):
+        print("Author Information:")
+        print("Author ID:", self.author_id)
+        print("Author Name:", self.author_name)
+        print("Affiliation:", self.affiliation)
+        print("Country:", self.country)
+        print("Phone:", self.phone)
+        print("Email:", self.email_id)
+
 
 # Main Code
-            print("Welcome!")
+books = {}
+authors = {}
+users = {}
 
-            while True:
-                print("Please choose one of the following: ")
-                print("1. Create object")
-                print("2. Project Closure")
-                print("3. Project Progress Update an employee")
-                print("4. Print a specific project")
-                print("4. Print all projects")
-                print("5. Quit Application")
+print("=Welcome")
 
-b1 = Book()
-b2 = Book()
-b3 = Book()
-b4 = Book()
-b5 = Book()
+while True:
+    print("1. Add Author")
+    print("2. Add Book")
+    print("3. Add User")
+    print("4. Borrow Book")
+    print("5. Display All Authors")
+    print("6. Display All Books")
+    print("7. Display All Users")
+    print("8. Quit")
 
-u1 = User()
-u2 = User()
+    option = input("Enter your choice: ")
 
+    if option == "1":
+        a = Author()
+        a.add_author()
+        authors[a.author_id] = a
+        print("Author", a.author_name, "added")
 
-u1.add_user()
-u2.add_user()
+    elif option == "2":
+        b = Book()
+        b.add_book()
+        if b.author_id in authors:
+            b.author = authors[b.author_id].author_name
+            books[b.bookID] = b
+            print("Book", b.book_title, "added")
+        else:
+            print("Author not found")
 
+    elif option == "3":
+        u = User()
+        u.add_user()
+        users[u.userID] = u
+        print("User", u.userName, "added")
 
-b1.add_book()
-b2.add_book()
-b3.add_book()
-b4.add_book()
-b5.add_book()
+    elif option == "4":
+        user_id = input("Enter User ID: ")
+        book_id = input("Enter Book ID: ")
+        if user_id in users and book_id in books:
+            users[user_id].borrow_book(books[book_id])
+        else:
+            print("User or Book not found")
 
+    elif option == "5":
+        if authors:
+            for a in authors.values():
+                a.display_author()
+        else:
+            print("No authors available")
 
-u1.borrow_book(b1)
-u1.borrow_book(b3)
-u2.borrow_book(b2)
-u2.borrow_book(b4)
-u2.borrow_book(b5)
+    elif option == "6":
+        if books:
+            for b in books.values():
+                b.display_book()
+        else:
+            print("No books available")
 
+    elif option == "7":
+        if users:
+            for u in users.values():
+                u.display_user()
+        else:
+            print("No users available")
 
-u1.display_user()
-u2.display_user()
+    elif option == "8":
+        print("Thank you for using the program")
+        break
